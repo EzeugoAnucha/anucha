80cca52d1757e4c7a5f3329cbd2febeee8c156c9
EzeugoAnucha <generalezeugo@gmail.com>
2023-12-14 15:16:55 +0100

    first commit

---
+++ EzeugoAnucha.py
@@ -0,0 +1,14 @@
+p = float(input("Enter initial principal: "))
+
+r = int(input("Enter interest rate: "))
+
+n = float(input("Enter number of times interest applied: "))
+
+t = float(input("Enter time period: "))
+
+SI = str(p * (1+r*t))
+CI = str(p * (1 + r/n) ** r * t)
+print("Yusuf and sons")
+print("SIMPLE INTEREST = " + SI)
+print("COMPOUND INTEREST = " + CI)
+
